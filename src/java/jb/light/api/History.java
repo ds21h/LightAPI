32ad875968cddfed96a1c66d5d8403a49f262f75
ds21h <ds21h@hotmail.com>
2021-06-03 16:56:13 +0200

    Version 1.1 - 202106003

--- src/java/jb/light/api/History.java
+++ src/java/jb/light/api/History.java
@@ -10,4 +10,6 @@ Version 1.0 - 09-12-2018
   -   Requires package jb.light.support
 Version 1.0.1 - 10-01-2019
   -   Bug in SettingR removed (getJSONObject replaced by optJSONObject)
+Version 1.1 - 03-06-2021
+  -   Uses upgraded LightSupport.jar (Version 1.1)
  */
